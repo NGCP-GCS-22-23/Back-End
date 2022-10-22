24de807d6587350cbd1328c4f9ba3bb3c175cabe
Dexter <dexterious56@gmail.com>
2022-10-22 09:44:36 -0700

    Added docstring for sampleGCS.py and test_packet.py

--- sampleGCS.py
+++ sampleGCS.py
@@ -1,216 +1,215 @@
-import json
-from tkinter import EW
-from vehicleDataFormat import *
-from datetime import datetime
-from vehicleDatabase import *
-from generalStage import *
-
-
 from digi.xbee.devices import DigiMeshDevice
-import xbee
-import threading
-from updateVehicle import *
-
-import os
-import time
-
-#from pymavlink import mavutil
-from xbee import TransmitThread, ToMAC, ToERU, ToGCS, Orientation, LatLng, ManualControl, Geofence, SearchArea, read_lock
+from pymavlink import mavutil
+from xbee import TransmitThread, ToMAC, ToGCS, Orientation, LatLng, ManualControl, Geofence, read_lock
 #import mavlink_msg_MAC
 
+from enum import IntEnum
+import threading
 import queue
 import struct
 import time
 
+class StatesMAC(IntEnum):
+	""" Initialize the device status constants 
 
-now = datetime.now()
-
-class updateDatabase():
-    def newEntries (gcsPacket, newestPacketTime, vehicleName):
-
-        newestPacketTime = str(newestPacketTime)
-
-        # Update the vehicle dictionary with given values 
-        requestedVehicle = updateVehicle.newAltitude(gcsPacket[0])
-        requestedVehicle = updateVehicle.newSpeed(gcsPacket[1])
-        requestedVehicle = updateVehicle.newRoll(gcsPacket[2])
-        requestedVehicle = updateVehicle.newPitch(gcsPacket[3])
-        requestedVehicle = updateVehicle.newYaw(gcsPacket[4])
-        requestedVehicle = updateVehicle.newLatitude(gcsPacket[5])
-        requestedVehicle = updateVehicle.newLongitude(gcsPacket[6])
-        requestedVehicle = updateVehicle.newBattery(gcsPacket[7])
-        requestedVehicle = updateVehicle.newSensorsOk(gcsPacket[8])
-        requestedVehicle = updateVehicle.newCurrentStage(gcsPacket[9])
-        requestedVehicle = updateVehicle.newStageCompleted(gcsPacket[10])
-        requestedVehicle = updateVehicle.newHikerPositionLat(gcsPacket[11])
-        requestedVehicle = updateVehicle.newHikerPositionLng(gcsPacket[12])
-        requestedVehicle = updateVehicle.newStatus(gcsPacket[13])
-        requestedVehicle = updateVehicle.newPropulsion(gcsPacket[14])
-        requestedVehicle = updateVehicle.newGeofenceCompilant(gcsPacket[15])
-        requestedVehicle = updateVehicle.newMode("Manual")
-        requestedVehicle = updateVehicle.newTimeSinceLastPacket(0)
-        requestedVehicle = updateVehicle.newLastPacketTime(0)
-        requestedVehicle = updateVehicle.newTime(str(now))
-        requestedVehicle = updateVehicle.newErrMsg("None")
-        #print(gcsPacket)
-        currentStage = requestedVehicle['current_stage']
-        stageName = updateStage.updateStageName(currentStage)
-        requestedVehicle = updateVehicle.newStageName(stageName)
-
-        # print(requestedVehicle)
-        vehicleDatabase.saveData(requestedVehicle, vehicleName)
+	Args:
+		IntEnum (_type_): (Deprecated)
+	"""
+	LOITER = 0          # Loiter at the current altitude in a circle
+	TAKEOFF = 1         # Switch to manual controls and takeoff
+	LOCATE_HIKER = 2    # Radio search for the hiker
+	ERU_DROP = 3        # Payload drop
+	LANDING = 9         # Switch to manual controls and land
 
 
-    
-class Xbee:
-    def xbee_run(xbee_data, send_flag, receive_flag, vehicleName, xbee_send_data, controller_data):
-        comm_port = "COM3" # can be swapped out for "/dev/ttyUSB0" for serial connection
-        baud_rate = "115200"
+class XBeeReceiver:
+	def __init__(self, num_items : int, device : DigiMeshDevice):
+		self.device = device                                # the managed xbee, used to receive data as a string of bytes (a.k.a. packet)
+		self.num_items = num_items                          # the size of the data and decode queues
+		self.packet_counter = 0                             # counter used anticipate the number of packets to be received
+		self.packet_buffer = []                             # temporary buffer used to combine multiple packets into one; necessary for when data
+								    # from the managed xbee exceeds the maximum packet size
+		self.data_queue = queue.Queue(num_items)            # queue used to hold the data to be deserialized
+		self.decode_queue = queue.Queue(num_items)          # the queue that holds decoded packets for the main script
+		self.transmit_thread = None                         # will be set to the managed transmit thread by the start_decode_thread() method
 
-        device = DigiMeshDevice(port=comm_port, baud_rate=baud_rate)
-        device.open()
+		# Set received packets to be placed in the packet buffer as soon as our xbee device receives them
+		self.device.add_data_received_callback(self.receive_packets)
+	
+	def receive_packets(self, packet):
+		"""Used to preprocess packets received from GCS and put them into a queue to await deserialization.
+		   Since one message from GCS may exceed the maximum packet length, multiple packets may be used to
+		   send over one message. The preprocessing done by this function collects all those packets up into
+		   one byte array, where it is then saved in data_queue as one complete message.
 
-        network = device.get_network()
-        network.start_discovery_process()
+		Args:
+			packet (_type_): data packet
+		"""
 
-        while network.is_discovery_running():
-            time.sleep(.01)
 
-        devices = {dev.get_node_id():dev._64bit_addr for dev in network.get_devices()}
-        devices[device.get_node_id()] = device._64bit_addr
+		if self.packet_counter == 0:
+			# Upon having received all packets, prepare the packet buffer to be stored in data_queue
+			self.packet_counter = struct.unpack("I", packet.data[:4])[0] -1 
+			data = packet.data[4:]
+			self.packet_buffer = b''
+		else:
+			# The entire series of packets containing the message has yet to be fully preprocessed,
+			# so put this packet's data into the buffer and go to the next packet
+			self.packet_counter -= 1
+			data = packet.data
+		
+		self.packet_buffer += data
 
-        print("This device's name: ", device.get_node_id())
-        print("Discovered ", devices)
+		# Upon having received all packets and the buffer has been prepared, store the data in the queue to
+		# await deserialization
+		if self.packet_counter == 0:
+			with read_lock:
+				self.data_queue.put(self.packet_buffer)
+				self.packet_buffer = []
 
-        geo_bounds = [Geofence(True, [LatLng(1,0),LatLng(0,1),LatLng(-1,0),LatLng(0,-1)])]
-        geo_bounds.append(Geofence(False, [LatLng(1,1),LatLng(2,1),LatLng(2,-1),LatLng(1,-1)]))
+	def decode_packet(self):
+		"""Decodes the next packet in the data queue if there is one.  Is passed to a TransmitThread object,
+		   which will run it in a loop for the lifetime of that object, by the start_decode_thread() method.
+		"""
 
-        area = SearchArea([LatLng(1,0),LatLng(0,1),LatLng(-1,0),LatLng(0,-1)])
+		try:
+			data = self.data_queue.get_nowait()
+			self.decode_queue.put(ToMAC.deserialize(data))
+		except queue.Empty:
+			# If the packet buffer is empty, do nothing
+			# The TransmitThread object will call this function in the next iteration of its loop,
+			# so nothing is done here to simply let the thread check again
+			pass
 
-        hiker_pos = LatLng(35.083519, -120.534821)
+	def start_decode_thread(self):
+		"""Creates the thread which constantly checks the encode queue for data to serialize and transmit.
+		"""
+		
+		self.transmit_thread = TransmitThread(self.decode_packet)
+		self.transmit_thread.start()
 
-        state = 1
-        stop = False
+	def __del__(self):
+		# Removes the function that places packets in the packet buffer from the list of functions called when the 
+		# managed xbee device receives data
+		self.device.del_data_received_callback(self.receive_packets)
 
-        global data_queue
-        global decode_queue 
-        data_queue = queue.Queue(9)  
-        decode_queue = queue.Queue(9)
+class XBeeSender:
+	def __init__(self, num_items : int, device : DigiMeshDevice):
+		self.device = device                                # the managed xbee, used to send data as a string of bytes (a.k.a. packet)
+		self.num_items = num_items                          # the size of the encode queue
+		self.encode_queue = queue.Queue(num_items)          # the queue that holds packets to be encoded and transmitted from the main script
+		                                                    # all items on the queue should be a tuple (id_of_recipient_xbee, data)
+		self.transmit_thread = None                         # will be set to the managed transmit thread by the start_encode_thread() method
+		
+	def encode_packet(self):
+		"""Serializes and transmits the next either ToMAC, ToGCS, ToERU, or ToMEA class found on the encode queue.
+		   Sends the data to the xbee specified by the id packaged with the data on the encode queue. Is passed to a 
+		   TransmitThread object, which will run it in a loop for the lifetime of that object, by the 
+		   start_decode_thread() method.
+		"""
 
-        def receive_packets(packet):
-            global packet_counter
-            global packet_buffer
-            packet_buffer = {}
-            packet_counter = {}
-            '''Used to preprocess packets received from GCS and put them into a queue to await deserialization.
-                Since one message from GCS may exceed the maximum packet length, multiple packets may be used to
-                send over one message. The preprocessing done by this function collects all those packets up into
-                one byte array, where it is then saved in data_queue as one complete message.'''
+		try:
+			data = self.encode_queue.get_nowait()
+			data[1].serialize().transmit(self.device, data[0])
+		except queue.Empty:
+			# If the encode queue is empty, do nothing
+			# The TransmitThread object will call this function in the next iteration of its loop,
+			# so nothing is done here to simply let the thread check again
+			pass
+	
+	def start_encode_thread(self):
+		"""Creates the thread which constantly checks the encode queue for data to serialize and transmit.
+		"""
+		
+		self.transmit_thread = TransmitThread(self.encode_packet)
+		self.transmit_thread.start()
 
-            if len(packet_counter) == 0:
-                # Upon having received all packets, prepare the packet buffer to be stored in data_queue
-                packet_counter = struct.unpack("I", packet.data[:4])[0] -1 
-                data = packet.data[4:]
-                packet_buffer = b''
-            else:
-                # The entire series of packets containing the message has yet to be fully preprocessed,
-                # so put this packet's data into the buffer and go to the next packet
-                packet_counter -= 1
-                data = packet.data
-        
-            packet_buffer += data
-            #print(self.packet_buffer)
-            #print("this is from the packet buffer")
-            # Upon having received all packets and the buffer has been prepared, store the data in the queue to
-            # await deserialization
-            if packet_counter == 0:
-                #print("data queue")
-                #print(len(self.packet_buffer))
-                with read_lock:
-                    data_queue.put(packet_buffer)
-                    packet_buffer = []
+def get_devices(device):
+	"""Get the devices name, ID, and other info
 
-        device.add_data_received_callback(receive_packets)
+	Args:
+		device (_type_): device
 
-        TIMEOUT = 1000
-        while True:
-            if send_flag.value == 1:
-                print("Hi")
-                send_flag.value = 0
-                state = 1
-                stop = False
-                cmd = ""
-                sent = False
-                if (vehicleName.value == "ERU"):
-                    cmd = "e"
-                if (vehicleName.value == "MAC"):
-                    cmd = "m"
-                # if (vehicleName.value == "MEA"):
-                #     cmd = "l"
-                #elif (vehicleName.value == "MAC"):
-                #    cmd = "m"
-                try:
-                    if cmd is "s":
-                        stop = not stop
-                    if cmd is "e":
-                        print("Sending to ERU")
-                        xbee.read_lock.acquire()
-                        ToERU(stop, state, hiker_pos, geo_bounds, LatLng(5,5), LatLng(5.5,5.5), False, 
-                            ManualControl(controller_data[0], controller_data[1], controller_data[2], 
-                                            controller_data[3], controller_data[4], controller_data[5], 
-                                            controller_data[6], controller_data[7]), True, True
-                            ).serialize().transmit(device, devices['eru'])
-                        xbee.read_lock.release()
-                    if cmd is "m":
-                        print("Sending to MAC")
-                        #device.del_data_received_callback(getPacket.packet_received)
-                        ToMAC(None, state, hiker_pos, geo_bounds, [area], LatLng(5,5), LatLng(5.5,5.5), True
-                            ).serialize().transmit(device, devices['mac'])
-                    # if cmd is "l":
-                    #     print("Sending to MEA")
-                    #     #device.del_data_received_callback(getPacket.packet_received)
-                    #     ToMEA(None, state, geo_bounds, LatLng(5,5), LatLng(5.5,5.5), False, True
-                    #         ).serialize().transmit(device, devices['mea'])
-                        #sent = True       
-                except KeyboardInterrupt:
-                    f = 0
-                    #device.del_data_received_callback(getPacket.packet_received)
-                current_time = int(round(time.time() * 1000))
-                xbee_end_time = current_time
-                xbee_start_time = current_time
-                while (xbee_end_time - xbee_start_time < TIMEOUT):
-                    xbee_end_time = int(round(time.time() * 1000))
-                    try:
-                        data = data_queue.get_nowait()
-                        if type(data) == bytes:
-                            receive_flag.value = 1
-                            decode_queue.put(ToGCS.deserialize(data))
-                            xbee_data_temp = decode_queue.get_nowait()
-                            xbee_data[0] = xbee_data_temp.altitude
-                            xbee_data[1] = xbee_data_temp.speed
-                            xbee_data[2] = xbee_data_temp.orientation.roll
-                            xbee_data[3] = xbee_data_temp.orientation.pitch
-                            xbee_data[4] = xbee_data_temp.orientation.yaw
-                            xbee_data[5] = xbee_data_temp.gps.lat
-                            xbee_data[6] = xbee_data_temp.gps.lng
-                            xbee_data[7] = xbee_data_temp.battery
-                            xbee_data[8] = xbee_data_temp.sensors_ok
-                            xbee_data[9] = xbee_data_temp.current_state
-                            xbee_data[10] = xbee_data_temp.state_complete
-                            xbee_data[11] = xbee_data_temp.hiker_position.lat
-                            xbee_data[12] = xbee_data_temp.hiker_position.lng
-                            xbee_data[13] = xbee_data_temp.status
-                            xbee_data[14] = xbee_data_temp.propulsion
-                            xbee_data[15] = xbee_data_temp.geofence_compliant
-                            xbee_data[16] = xbee_data_temp.manual_mode
-                            TIMEOUT = 1000
-                            break
-                            #print(xbee_data_temp)
+	Returns:
+		_type_: device
+	"""
+	print("Getting devices")
 
-                            #print(self.decode_queue.qsize())
-                    except queue.Empty:
-                        TIMEOUT = 1000
-                        # print("hi")
-                        # If the packet buffer is empty, do nothing
-                        # The TransmitThread object will call this function in the next iteration of its loop,
-                        # so nothing is done here to simply let the thread check again
+	network = device.get_network()
+	network.start_discovery_process()
+
+	while network.is_discovery_running():
+		time.sleep(.01)
+	
+	devices = {dev.get_node_id():dev._64bit_addr for dev in network.get_devices()}
+	devices[device.get_node_id()] = device._64bit_addr
+
+	print("This device's name: ", device.get_node_id())
+	print("Discovered ", devices)
+
+	return devices
+
+def main():
+	"""Establish communication with the device and initialize the XBeeReceiver and XBeeSender classes
+       Display the current status of the device based on the defined constants
+	   
+	"""
+	# Set up communication devices
+	comm_port = "/dev/ttyUSB0"
+	baud_rate = "9600"
+
+	device = DigiMeshDevice(port=comm_port, baud_rate=baud_rate)
+	device.open()
+
+	devices = get_devices(device)
+
+	num_items = 9
+	#decoded_packets = queue.Queue(num_items)  # buffer of classes holding decoded packet data FIFO
+	#encoded_packets = queue.Queue(num_items)  # buffer of data to encode to gcs FIFO
+	
+	# initialize constantly-run threads
+	xbee_receiver = XBeeReceiver(num_items, device)
+	xbee_sender = XBeeSender(num_items, device)
+
+	# start constantly-run threads
+	xbee_receiver.start_decode_thread()
+	xbee_sender.start_encode_thread()
+	#xbee_decode_thread = TransmitThread(xbee_receiver.decode_packet)
+	#xbee_decode_thread.start()
+
+	while True:
+		# Check if data has been decoded and sent back to main script queue
+		try:
+			gcs_cmd = xbee_receiver.decode_queue.get_nowait()
+			
+			# series of conditionals for event-based functions
+			if gcs_cmd.perform_state == StatesMAC.LOITER:
+				print("Loiter at the current altitude in a circle")
+			elif gcs_cmd.perform_state == StatesMAC.TAKEOFF:
+				print("Switch to manual controls and takeoff")
+			elif gcs_cmd.perform_state == StatesMAC.LOCATE_HIKER:
+				print("Radio search for the hiker")
+			elif gcs_cmd.perform_state == StatesMAC.ERU_DROP:
+				print("Payload drop")
+			elif gcs_cmd.perform_state == StatesMAC.LANDING:
+				print("Switch to manual controls and land")
+			else:
+				print("not a recognized state")
+			
+		except queue.Empty:
+			pass
+		finally:
+			pass
+		# check on constantly run functions
+		# check if data needs to be encoded and place in queue
+		cmd = input("Press y to send packet to GCS\n")
+		if cmd == "y":
+			recipient = devices['gcs']
+			data = ToGCS(50.0, 80.0, Orientation(20.0, 120.0, 270.0), LatLng(35.2, 43.5), 0.90, True, 3, False, LatLng(21.4, 8.0), 4, True, True)
+			xbee_sender.encode_queue.put((recipient, data))
+
+	# Close all communication
+	device.close()
+
+if __name__ == "__main__":
+	main()
